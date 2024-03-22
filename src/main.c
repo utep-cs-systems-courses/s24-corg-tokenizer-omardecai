2ab272e89fe8519d94303a69643a6d549d82bd61
Edgar Arellano <edgararellano@Edgars-Air-3.lan>
2024-03-22 12:59:32 -0600

    main.c debug

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 #include "tokenizer.h"
 #include "history.h"
 
-#define MAX_INPUT_LENGTH 1000
+#define MAX_INPUT_LENGTH 100
 
 int main(){
-  char input[MAX_INPUT_LENGTH];
+  char str[MAX_INPUT_LENGTH];
 
   while (1) {
     
@@ -14,36 +14,34 @@ int main(){
     printf("> ");
 
     //Read user input
-    int i = 0;
-    char c;
+    //    int i = 0;
+    //  char c;
 
-    while ((c = getchar()) != '\n' && i < MAX_INPUT_LENGTH - 1) {
-      if (space_char(c)) {
-	if (i > 0 && !space_char(input[i-1])){
-	  input[i++] = c;
-	}
-      } else {
-	input[i++] = c;
-      }
-    }
-    input[i] = '\0';
+    //  while ((c = getchar()) != '\n' && i < MAX_INPUT_LENGTH - 1) {
+    //  if (space_char(c)) {
+    //	if (i > 0 && !space_char(input[i-1])){
+    //	  input[i++] = c;
+    //	}
+    //  } else {
+    //	input[i++] = c;
+    //  }
+    //  }
+    //  input[i] = '\0';
     
+    fgets(str, sizeof(str), stdin);
     
     //Echo input
-    printf("%s\n", input);
+    printf("%s\n", str);
 
     //tokenize input
     List* history = init_history();
-    add_history(history, input);
+    add_history(history, str);
     
-    char **tokens = tokenize(input);
-    if (tokens == NULL) {
-      printf("Failed to tokenize input\n");
-      continue;
-    }
+    char **tokens = tokenize(str);
 
-    printf("Tokens:\n");
-    print_tokens(tokens);
+    for (int i = 0; tokens[i] != NULL; i++) {
+      printf("Tokens: %s", tokens[i]);
+    }
     
     free_tokens(tokens);
   }
